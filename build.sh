209d247d200e923a8eeabb0a6f3549549513da4b
Abhay Iyer <abhay.iyer6@gmail.com>
2020-06-14 10:32:28 +0530

    c++ build file

---
+++ build.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+for fn in `find . -name "*.cpp"`; do 
+	echo "Currently compiling $fn"
+	g++ $fn
+done
+
+count=`find . -name "*.cpp" | wc -l`
+
+echo ""
+echo "=============================="
+echo "         Build Details        " 
+echo "       Compiled $count files    "
+echo "=============================="
